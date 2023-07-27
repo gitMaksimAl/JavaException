5d0dc244a7ebeccc1f6bfc563862f13f67550918
Maksim Aleksanin <maksim.g00gle@gmail.com>
2023-07-27 23:38:41 +0400

    Homework 3, full worked model

--- Homework_3/Main.java
+++ Homework_3/Main.java
@@ -1,7 +1,27 @@
 package Homework_3;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import Homework_3.RequestExceptions.ParseException;
+import Homework_3.RequestExceptions.WrongDataException;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Please enter user data like: " +
+            "<Name> <Lastname> <Surname> <birthday(dd.mm.yyyy)> " +
+            "<phonenumber(11 digit)> <sex(m|f)>");
+        try (Scanner scanner = new Scanner(System.in)){
+            String[] userData = User.getData(scanner.nextLine());
+            User userOne = new User(userData);
+            userOne.validate();
+            System.out.println(userOne.getFile());
+        } catch (ParseException p) {
+            System.err.println(p.getMessage());
+        } catch (WrongDataException w) {
+            System.err.println(w.getMessage());
+        } catch (NoSuchElementException io) {
+            System.err.println(io.getMessage());
+        }
     }
 }
--- Homework_3/User.java
+++ Homework_3/User.java
@@ -1,22 +1,30 @@
 package Homework_3;
 
 import java.io.File;
-import RequestExceptions;
 import Homework_3.RequestExceptions.ParseException;
+import Homework_3.RequestExceptions.WrongDataException;
 
+/**
+ * User.
+ * Use static method 'createUser' for create new user from string with data.
+ */
 public class User {
 
     private Name userName;
     private BirthDay bday;
     private PhoneNumber phone;
     private char sex;
+    private String file;
 
     public User() {this.userName = null;}
 
     public static String[] getData(String data) throws ParseException{
         String[] parsed = data.split(" ");
         if (parsed.length != 6) throw new RequestExceptions.ParseException();
-        else return parsed;
+        else {
+            for (String item: parsed) System.out.println(item);
+            return parsed;
+        }
     }
 
     public User(String[] data) {
@@ -26,6 +34,34 @@ public class User {
         this.sex = data[5].charAt(0);
     }
 
+    public void validate() throws WrongDataException{
+        try {
+            this.userName.valid();
+            this.bday.valid();
+            this.phone.valid();
+            if (this.sex == 'f' || this.sex == 'm')
+                this.file = String.format("%s%s%s", 
+                    System.getProperty("user.dir"),
+                    File.separator,
+                    this.userName.lastName);
+            else throw new RequestExceptions.WrongDataException();
+        } catch (WrongDataException w) {
+            throw w;
+        }
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s %c",
+            this.userName,
+            this.bday,
+            this.phone,
+            this.sex);
+    }
     /**
      * Name
      */
@@ -37,15 +73,23 @@ public class User {
         private String surName;
 
         private Name(String fname, String lname, String sname) {
-            this.firstName = fname;
             this.lastName = lname;
+            this.firstName = fname;
             this.surName = sname;
         }
 
-        private boolean isCorrect() {
-            return (this.firstName.matches(regex)
+        private void valid() throws WrongDataException{
+            if (!this.firstName.matches(regex)
                 && this.lastName.matches(regex)
-                && this.surName.matches(regex));
+                && this.surName.matches(regex))
+                    throw new RequestExceptions.WrongDataException();
+        }
+
+        public String toString() {
+            return String.format("%s %s %s",
+                this.firstName,
+                this.lastName,
+                this.surName);
         }
     }
 
@@ -61,22 +105,24 @@ public class User {
             this.date = date;
         }
 
-        private boolean isCorrect() {
-            return this.date.matches(regex);
+        private void valid() throws WrongDataException{
+            if (!this.date.matches(regex))
+                throw new RequestExceptions.WrongDataException();
         }
     }
 
     public class PhoneNumber {
 
-        private String regex = "^\\d{2}.\\d{2}.\\d{4}$";
+        private String regex = "^\\d{11}";
         private String number;
 
         private PhoneNumber(String number) {
             this.number = number;
         }
 
-        private boolean isCorrect() {
-            return number.matches(regex);
+        private void  valid() throws WrongDataException{
+            if (!number.matches(regex))
+                throw new RequestExceptions.WrongDataException();
         }
     }
 }
